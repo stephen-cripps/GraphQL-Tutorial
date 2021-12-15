a25b2d7f901a7aa6b746356785ad9fb24a197947
Stephen Cripps <stephen.cripps@tutanota.com>
2021-12-15 15:18:51 +0000

    Hello World

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 const express = require("express");
+const expressGraphQl = require("express-graphql").graphqlHTTP;
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+
 const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Hello_World",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "Hello World",
+      },
+    }),
+  }),
+});
+
+app.use(
+  "/graphql",
+  expressGraphQl({
+    schema,
+    graphiql: true,
+  })
+);
 app.listen(5000, () => console.log("Server Running"));
